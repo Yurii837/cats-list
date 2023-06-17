1414e45730cd73355a7f0a4348ada1035e934013
Yurii837 <gorogotskiy@gmail.com>
2023-06-17 09:37:36 +0300

    add forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CatsServise } from './cats.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'cats-list';
+export class AppComponent implements OnInit, OnDestroy {
+ 
+  constructor (
+    public catsServise: CatsServise
+    ) { }
+
+  public formGroup = new FormGroup ({
+    SearchQuery: new FormControl<string | null>(null,  [Validators.maxLength(10)]),
+    Breed: new FormControl(null),
+  })
+  
+  ngOnInit(): void {
+    
+  }
+
+  ngOnDestroy(): void {
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './Modules/material-module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
